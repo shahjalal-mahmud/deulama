4690dd9c29a6aac09ed33c3285282c96541f97b4
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-12 02:01:47 +0600

    Add authentication module and view model to dependency injection

--- app/src/main/java/com/appriyo/deulama/di/AppModules.kt
+++ app/src/main/java/com/appriyo/deulama/di/AppModules.kt
@@ -2,13 +2,20 @@ package com.appriyo.deulama.di
 
 import com.appriyo.deulama.data.local.datastore.SessionManager
 import com.appriyo.deulama.data.remote.networkModule
+import com.appriyo.deulama.data.repository.AuthRepositoryImpl
+import com.appriyo.deulama.domain.repository.AuthRepository
+import com.appriyo.deulama.presentation.auth.AuthViewModel
 import com.appriyo.deulama.presentation.home.HomeViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 private val appOnlyModule = module {
     single { SessionManager(get()) }
-    viewModel { HomeViewModel(get()) }
+
+    single<AuthRepository> { AuthRepositoryImpl(get(), get(), get()) }
+
+    viewModel { AuthViewModel(get()) }
+    viewModel { HomeViewModel(get(), get()) }
 }
 
 val appModules = listOf(
