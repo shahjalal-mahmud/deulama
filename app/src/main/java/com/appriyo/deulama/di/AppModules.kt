4f9244c708b2894148826408a77522b68fefa00e
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-12 03:40:08 +0600

    Add repository implementations and view models for swipe and favorites features

--- app/src/main/java/com/appriyo/deulama/di/AppModules.kt
+++ app/src/main/java/com/appriyo/deulama/di/AppModules.kt
@@ -1,27 +1,43 @@
 package com.appriyo.deulama.di
 
+import com.appriyo.deulama.data.local.datastore.AppPrefs
 import com.appriyo.deulama.data.local.datastore.SessionManager
 import com.appriyo.deulama.data.remote.networkModule
 import com.appriyo.deulama.data.repository.AuthRepositoryImpl
 import com.appriyo.deulama.data.repository.DramaRepositoryImpl
+import com.appriyo.deulama.data.repository.FavoritesRepositoryImpl
+import com.appriyo.deulama.data.repository.SwipeRepositoryImpl
+import com.appriyo.deulama.data.repository.WatchLaterRepositoryImpl
+import com.appriyo.deulama.data.repository.WatchedRepositoryImpl
 import com.appriyo.deulama.domain.repository.AuthRepository
 import com.appriyo.deulama.domain.repository.DramaRepository
+import com.appriyo.deulama.domain.repository.FavoritesRepository
+import com.appriyo.deulama.domain.repository.SwipeRepository
+import com.appriyo.deulama.domain.repository.WatchLaterRepository
+import com.appriyo.deulama.domain.repository.WatchedRepository
 import com.appriyo.deulama.presentation.auth.AuthViewModel
 import com.appriyo.deulama.presentation.details.DramaDetailsViewModel
 import com.appriyo.deulama.presentation.discover.DiscoverViewModel
+import com.appriyo.deulama.presentation.discover.SwipeDeckViewModel
 import com.appriyo.deulama.presentation.home.HomeViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 private val appOnlyModule = module {
     single { SessionManager(get()) }
+    single { AppPrefs(get()) }
 
     single<AuthRepository> { AuthRepositoryImpl(get(), get(), get()) }
     single<DramaRepository> { DramaRepositoryImpl(get(), get()) }
+    single<SwipeRepository> { SwipeRepositoryImpl(get(), get()) }
+    single<FavoritesRepository> { FavoritesRepositoryImpl(get(), get()) }
+    single<WatchLaterRepository> { WatchLaterRepositoryImpl(get(), get()) }
+    single<WatchedRepository> { WatchedRepositoryImpl(get(), get()) }
 
     viewModel { AuthViewModel(get()) }
     viewModel { HomeViewModel(get(), get(), get()) }
     viewModel { DiscoverViewModel(get()) }
+    viewModel { SwipeDeckViewModel(get(), get(), get(), get()) }
     viewModel { DramaDetailsViewModel(get()) }
 }
 
