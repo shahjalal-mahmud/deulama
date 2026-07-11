3bc6526602a12daeeb557df7acf78a486d794a4b
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-11 22:29:01 +0600

    Update MainActivity imports for navigation.
    Key changes include:
    - **Navigation**: Added the import for `HangugNavGraph` to prepare for navigation integration within `MainActivity`.

--- app/src/main/java/com/appriyo/deulama/MainActivity.kt
+++ app/src/main/java/com/appriyo/deulama/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.appriyo.deulama.presentation.navigation.HangugNavGraph
 import com.appriyo.deulama.ui.theme.HangugDeulamaTheme
 
 class MainActivity : ComponentActivity() {
