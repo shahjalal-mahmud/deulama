8f07ccfeb481d72093b8aa0d8d26d85bf9e06d87
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-12 17:06:42 +0600

    Add kotlinx-coroutines-test dependency for testing coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,7 @@ dependencies {
     ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
